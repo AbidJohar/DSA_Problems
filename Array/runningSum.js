000d971e75904f2b7d0f4107784ce31adc50e85b
Javascript_Developer <ah1502651@gmail.com>
2026-04-29 22:56:41 +0500

    Solve the runningSUm problem

---
+++ Array/runningSum.js
@@ -0,0 +1,36 @@
+
+
+//__________( Brute force Solution )________________
+
+// var runningSum = function(nums) {
+
+// let resultArr = [];
+
+//  for(let i=0; i< nums.length; i++){
+//     let sum = 0;
+//     for(let j=0; j<=i; j++){
+        
+//         sum += nums[j];
+ 
+//     }
+//     resultArr.push(sum);
+//  }
+//  return resultArr;
+// };
+
+// console.log(runningSum([1,2,3,4]));
+
+//_____________( Optimal Solution )_______________
+
+var runningSum = function(nums) {
+
+let sum=0;
+ for(let i=0; i< nums.length; i++){
+     sum += nums[i];
+     nums[i] = sum;
+    
+ }
+  return nums;
+};
+
+runningSum([1,2,3,4]);
